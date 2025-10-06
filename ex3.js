4c847f2e9518ea3525b2680fa86fa5f47ad40502
ashanhq <20064191@mydbs.ie>
2025-10-06 15:02:37 +0530

    Update ex3.js

--- ex3.js
+++ ex3.js
@@ -1,24 +1,65 @@
-let listEuler1=(a,b,l)=>0;//obviously wrong - compute the sum of multiples of a or b in list l
-let listEuler2=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
-let listEuler3=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
+// Compute the sum of numbers in list l that are multiples of a or b
+let listEuler1 = (a, b, l) => {
+    let sum = 0;
+    for (let num of l) {
+        if (num % a === 0 || num % b === 0) {
+            sum += num;
+        }
+    }
+    return sum;
+};
 
-let eulerlist=()=>
-{
-    a=2//can un-hardcode
-    b=3
-    l=[1,2,3,4,5,6,7,9,10,10,10]  //document.getElementById('l').value.split(" ").map((x=>parseInt(x)); // if l is space separated numbers
-    //call listEuler1 and alert. - compute the sum of multiples of a or b in list l
-}
-let euler2Lists=()=>
-{
-    a=[2,3]//can un-hardcode
-    l=[1,2,3,4,5,6,7,9,10,10,10]
-    //call listEuler2 and alert.
-}
+// Compute the sum of numbers in list l that are multiples of any element in list a
+let listEuler2 = (a, l) => {
+    let sum = 0;
+    for (let num of l) {
+        for (let div of a) {
+            if (num % div === 0) {
+                sum += num;
+                break; // prevent counting the same number twice
+            }
+        }
+    }
+    return sum;
+};
 
-let euler2Lists1=()=>
-{
-    a=[2,3,5]//can un-hardcode
-    l=[1,2,3,4,5,6,7,9,10,10,10]
-    //call listEuler3 and alert.
-}
+// Same as listEuler2 but supports any length of list a (generalized)
+let listEuler3 = (a, l) => {
+    let sum = 0;
+    for (let num of l) {
+        for (let div of a) {
+            if (num % div === 0) {
+                sum += num;
+                break;
+            }
+        }
+    }
+    return sum;
+};
+
+// ---- Event Functions ----
+
+// Exercise 3 - fixed and working
+let eulerlist = () => {
+    let a = 2;
+    let b = 3;
+    let l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10];
+    let result = listEuler1(a, b, l);
+    alert(`Sum of multiples of ${a} or ${b} in list l = ${result}`);
+};
+
+// Part 1: list a of length 2
+let euler2Lists = () => {
+    let a = [2, 3];
+    let l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10];
+    let result = listEuler2(a, l);
+    alert(`Sum of multiples of [${a}] in list l = ${result}`);
+};
+
+// Part 2: list a of any length
+let euler2Lists1 = () => {
+    let a = [2, 3, 5];
+    let l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10];
+    let result = listEuler3(a, l);
+    alert(`Sum of multiples of [${a}] in list l = ${result}`);
+};
